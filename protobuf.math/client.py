c2b3ffc61dd4b91ac3e1584aa1574243f76894b1
Ana Rubio Ruiz <Ana.Rubio@uclm.es>
2025-10-06 07:44:30 +0200

    protobuf.math: solution

--- protobuf.math/client.py
+++ protobuf.math/client.py
@@ -1,1 +1,29 @@
-#ToDo
+#!/usr/bin/python3
+"Usage: {0} <server_host:port> {{add|mul}} <n1> <n2> [n3 ...]"
+
+import sys
+import socket
+import math_pb2 as math
+
+
+if len(sys.argv) < 2:
+    print(__doc__.format(sys.argv[0]))
+    sys.exit(1)
+
+host, port = sys.argv[1].split(':')
+port = int(port)
+operation = sys.argv[2]
+numbers = [int(x) for x in sys.argv[3:]]
+
+with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+    request = math.Request()
+    request.numbers.extend(numbers)
+    request.operation = 0 if operation == 'sum' else 1
+
+    s.sendto(request.SerializeToString(), (host, port))
+
+    data = s.recv(4096)
+    response = math.Response()
+    response.ParseFromString(data)
+
+    print(f"Result: {response.result}")
--- protobuf.math/server.py
+++ protobuf.math/server.py
@@ -1,1 +1,28 @@
-#ToDo
+#!/usr/bin/python3
+import socket
+import math_pb2 as math
+
+
+def mul(numbers):
+    result = 1
+    for n in numbers:
+        result *= n
+    return result
+
+
+with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+    s.bind(('', 10001))
+
+    while 1:
+        print("\nWaiting for a request...")
+        data, client = s.recvfrom(4096)
+
+        request = math.Request()
+        request.ParseFromString(data)
+
+        calc = sum if request.operation == math.Request.SUM else mul
+        result = calc(request.numbers)
+
+        response = math.Response(result=result)
+        s.sendto(response.SerializeToString(), client)
+        print(f"{client} :: {request.operation} result = {result}")
