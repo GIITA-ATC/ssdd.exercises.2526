eeaa162cdd1bb8c533bdd2369b05c1523dc2c20e
Ana Rubio Ruiz <Ana.Rubio@uclm.es>
2025-10-27 07:30:50 +0100

    add grpc.math solution

--- grpc.math/client.py
+++ grpc.math/client.py
@@ -1,1 +1,26 @@
-#ToDo
+#!/usr/bin/env python3
+"Usage: {0} <server_host:port> {{add|mul}} <n1> <n2> [n3 ...]"
+
+import sys
+import grpc
+
+import math_pb2
+import math_pb2_grpc
+
+
+if len(sys.argv) < 2:
+    print(__doc__.format(sys.argv[0]))
+    sys.exit(1)
+
+server = sys.argv[1]
+operation = sys.argv[2]
+numbers = [int(x) for x in sys.argv[3:]]
+
+channel = grpc.insecure_channel(server)
+stub = math_pb2_grpc.MathStub(channel)
+
+request = math_pb2.Request(numbers=numbers)
+calc = stub.add if operation == 'add' else stub.multiply
+response = calc(request)
+
+print(f'Result: {response.result}')
--- grpc.math/server.py
+++ grpc.math/server.py
@@ -1,1 +1,32 @@
-#ToDo
+#!/usr/bin/env python3
+
+import grpc
+from concurrent import futures
+
+import math_pb2
+import math_pb2_grpc
+
+
+class MathService(math_pb2_grpc.MathServicer):
+    def add(self, request, context):
+        result = sum(request.numbers)
+        return math_pb2.Response(result=result)
+
+    def multiply(self, request, context):
+        result = 1
+        for n in request.numbers:
+            result *= n
+        return math_pb2.Response(result=result)
+
+
+server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+math_pb2_grpc.add_MathServicer_to_server(MathService(), server)
+server.add_insecure_port('[::]:10001')
+server.start()
+
+try:
+    print("\nWaiting for a request...")
+    server.wait_for_termination()
+
+except KeyboardInterrupt:
+    server.stop(0)
